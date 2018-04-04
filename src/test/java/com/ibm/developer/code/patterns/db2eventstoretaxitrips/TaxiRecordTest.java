aabafe6333065d6a4980375346b1316a2260d275
Mark Sturdevant <mark.sturdevant@ibm.com>
2018-04-03 18:01:16 -0700

    Add unit tests and copyrights.
    Simple tests for things that don't need mock.
    Copyright headers.

--- src/test/java/com/ibm/developer/code/patterns/db2eventstoretaxitrips/TaxiRecordTest.java
+++ src/test/java/com/ibm/developer/code/patterns/db2eventstoretaxitrips/TaxiRecordTest.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright 2018 IBM Corp. All Rights Reserved.
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.ibm.developer.code.patterns.db2eventstoretaxitrips;
 
 import junit.framework.Test;
@@ -7,7 +20,7 @@ import junit.framework.TestSuite;
 /** Unit test for simple TaxiRecord. */
 public class TaxiRecordTest extends TestCase {
   /**
-   * Create the test case
+   * Test the constructor and toString.
    *
    * @param testName name of the test case
    */
@@ -20,8 +33,8 @@ public class TaxiRecordTest extends TestCase {
     return new TestSuite(TaxiRecordTest.class);
   }
 
-  /** Rigourous Test :-) */
-  public void testTest() {
-    assertTrue(true);
+  public void testTaxiRecordDefaults() {
+    TaxiRecord tr = new TaxiRecord();
+    assertEquals(tr.toString(), "null|null|null|null|null|null|null|null|null|null|null");
   }
 }
